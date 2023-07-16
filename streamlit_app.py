dcde04674a69ac085a2349e7a71b5c89ab3c1441
Marina Nakagawa <mari10172001@yahoo.co.jp>
2023-07-16 22:49:51 +0900

    stlimlitをちょっと動かすまで

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,27 @@
 import streamlit as st
-st.write("Hello world. Let's learn how to build a AI-based app together.")
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import (SystemMessage, HumanMessage, AIMessage)
+
+
+def main():
+    st.set_page_config(
+        page_title="My Great ChatGPT",
+        page_icon="🤗"
+    )
+    st.header("My Great ChatGPT 🤗")
+    # input
+    if user_input := st.chat_input("聞きたいことを入力してね！"):
+        print("hoge")
+    
+    # input
+    container = st.container()
+    with container:
+        with st.form(key='my_form', clear_on_submit=True):
+            user_input = st.text_area(label="ききたいことを入力してね:",key="input",height=100)
+            submit_button = st.form_submit_button(label="Send")
+        if submit_button and user_input:
+            # なにか入力されればここが実行される
+            print("hogehogehgoe")
+
+if __name__ == '__main__':
+    main()
